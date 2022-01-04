74ce17dfb43c04a7c7a915344525d85d825ac939
Wouter Vernaillen <wouter@vernaillen.com>
2022-01-04 09:00:05 +0100

    version bumps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
-
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       screens: {
-        '3xl': '1760px',
+        "3xl": "1760px",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
