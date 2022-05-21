68b35c71558b85db57f90d6a8f9bad48634f3627
Wouter Vernaillen <wouter@vernaillen.com>
2022-05-21 11:27:12 +0200

    vite-tsconfig-paths not needed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tsconfigPaths from "vite-tsconfig-paths";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tsconfigPaths(), WindiCSS()],
+  plugins: [vue(), WindiCSS()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
