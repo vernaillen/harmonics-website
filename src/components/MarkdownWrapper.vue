f4d589436544bb5a08c8de07f8c8eeb543239d98
Wouter Vernaillen <wouter@vernaillen.com>
2022-07-24 20:15:39 +0200

    handleAnchors

--- src/components/MarkdownWrapper.vue
+++ src/components/MarkdownWrapper.vue
@@ -6,6 +6,51 @@ defineProps({
     required: true,
   },
 })
+const router = useRouter()
+const route = useRoute()
+const content = ref<HTMLDivElement>()
+onMounted(() => {
+  const navigate = () => {
+    if (location.hash) {
+      document.querySelector(decodeURIComponent(location.hash))
+        ?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+  const handleAnchors = (
+    event: MouseEvent & { target: HTMLElement },
+  ) => {
+    const link = event.target.closest('a')
+    if (
+      !event.defaultPrevented
+      && link
+      && event.button === 0
+      && link.target !== '_blank'
+      && link.rel !== 'external'
+      && !link.download
+      && !event.metaKey
+      && !event.ctrlKey
+      && !event.shiftKey
+      && !event.altKey
+    ) {
+      const url = new URL(link.href)
+      if (url.origin !== window.location.origin)
+        return
+      event.preventDefault()
+      const { pathname, hash } = url
+      if (hash && (!pathname || pathname === location.pathname)) {
+        window.history.replaceState({}, '', hash)
+        navigate()
+      }
+      else {
+        router.push({ path: pathname, hash })
+      }
+    }
+  }
+  useEventListener(window, 'hashchange', navigate)
+  useEventListener(content.value!, 'click', handleAnchors, { passive: false })
+  navigate()
+  setTimeout(navigate, 500)
+})
 </script>
 
 <template>
@@ -21,7 +66,9 @@ defineProps({
               {{ frontmatter.title_nl }}
             </h1>
           </div>
-          <slot />
+          <article ref="content">
+            <slot />
+          </article>
         </div>
       </div>
     </div>
