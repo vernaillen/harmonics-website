3554741c2f2da9aa511b12b6b5b0b1e00a0af171
Wouter Vernaillen <wouter@vernaillen.com>
2022-10-01 09:24:17 +0200

    fix lint issue in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
   video: false,
   e2e: {
     baseUrl: 'https://harmonics.be',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 })
