b13fd0314480397fd9bfd63fa45730a972fe4fd8
Wouter Vernaillen <wouter@vernaillen.com>
2022-10-01 15:06:12 +0200

    separate build & test jobs, no deploy job

--- cypress/e2e/testdraftblogpost.cy.ts
+++ cypress/e2e/testdraftblogpost.cy.ts
@@ -5,6 +5,6 @@ describe('check draft post', () => {
 
   it('displays title & taglines', () => {
     cy.get('article > .markdown-body p').should('have.length', 3)
-    cy.get('article > .markdown-body p').last().should('have.text', 'En nog een extra tekstje om cypress e2e nogmaals te testen')
+    cy.get('article > .markdown-body p').last().should('have.text', 'En nog een extra tekstje')
   })
 })
