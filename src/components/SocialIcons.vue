b4e2d508252c36afbce5f875bb9b6c8590dc9740
Wouter Vernaillen <wouter@vernaillen.com>
2022-11-20 15:39:23 +0100

    mastodon

--- src/components/SocialIcons.vue
+++ src/components/SocialIcons.vue
@@ -4,18 +4,17 @@ const iconClass = 'hover:text-primary text-gray-500 h-5 mx-2'
 
 <template>
   <div id="socials" class="min-w-[110px] pt-1 sm:mr-4 xl:pr-0 mr-[-10px]">
-    <a
-      href="https://www.youtube.com/@harmonicsbe" target="_blank" rel="noopener"
-    >
+    <a href="https://www.youtube.com/@harmonicsbe" target="_blank" rel="noopener">
       <FontAwesomeIcon :icon="['fab', 'youtube']" :class="iconClass" />
     </a>
-    <a
-      href="https://www.facebook.com/harmonics.be/" target="_blank" rel="noopener"
-    >
+    <a href="https://www.facebook.com/harmonics.be/" target="_blank" rel="noopener">
       <FontAwesomeIcon :icon="['fab', 'facebook']" :class="iconClass" />
     </a>
     <a href="https://www.instagram.com/harmonics.be/" target="_blank" rel="noopener">
       <FontAwesomeIcon :icon="['fab', 'instagram']" :class="iconClass" />
+    </a>
+    <a href="https://fosstodon.org/@vernaillen" rel="me" target="_blank">
+      <font-awesome-icon :icon="['fab', 'mastodon']" />
     </a>
   </div>
 </template>
