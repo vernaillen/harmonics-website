0de9b374ceadce715c7102154e30d3d3d3c5dbae
Wouter Vernaillen <wouter@vernaillen.com>
2023-01-19 08:09:04 +0100

    fix thanks page e2e test

--- cypress/e2e/thankspage.cy.ts
+++ cypress/e2e/thankspage.cy.ts
@@ -6,11 +6,12 @@ describe('test thanks page', () => {
   })
 
   it('Dutch page has correct content', () => {
-    cy.get('main h1').should('have.length', 1)
+    cy.get('main h1').should('have.length', 2)
     cy.get('main h1').first().should('have.text', 'Bedankt voor jouw bericht!')
+    cy.get('main h1').last().should('have.text', 'Ontvang Harmonics nieuws via e-mail')
 
     cy.get('main article').should('have.length', 1)
-    cy.get('main article > div > p').first().should('have.text', 'Ik laat je zo snel mogelijk iets weten')
+    cy.get('main article > div > div > p').first().should('have.text', 'Ik laat je zo snel mogelijk iets weten')
   })
 
   it('English page has correct content', () => {
@@ -21,11 +22,11 @@ describe('test thanks page', () => {
       expect(location.pathname).to.eq('/en/thanks/')
     })
 
-    /* cy.get('main h1').should('have.length', 1)
+    cy.get('main h1').should('have.length', 2)
     cy.get('main h1').first().should('have.text', 'Thanks for your message!')
+    cy.get('main h1').last().should('have.text', 'Receive Harmonics news by email')
 
     cy.get('main article').should('have.length', 1)
-    cy.get('main article > div > p').first().should('have.text', 'I\'ll get back to you soon') */
+    cy.get('main article > div > div > p').first().should('have.text', 'I\'ll get back to you soon')
   })
 })
-
