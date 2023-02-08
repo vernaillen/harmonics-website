a6ca69975a0dbb5a000edc947506d1f437f56f20
Wouter Vernaillen <wouter@vernaillen.com>
2023-02-08 16:16:20 +0100

    reserve your spot button

---
+++ components/content/ButtonLink.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  src: string
+  extraClass?: string
+}>()
+</script>
+
+<template>
+  <a :href="src" target="_blank" class="hover:!border-0" :class="extraClass">
+    <button class="text-base font-medium text-white bg-primary py-1 px-3 m-2 hover:bg-opacity-80 hover:shadow-signUp rounded-md">
+      {{ text }}
+    </button>
+  </a>
+</template>
