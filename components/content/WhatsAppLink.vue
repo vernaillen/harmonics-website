5a6bb2a3e1397b609fdba6e48ae3124b07f827aa
Wouter Vernaillen <wouter@vernaillen.com>
2023-02-09 12:18:37 +0100

    changed image filenames, hoping twicpics will reload them

---
+++ components/content/WhatsAppLink.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <a href="https://wa.me/+32473344656" target="_blank">
+    <Icon name="logos:whatsapp-icon" class="hover:text-gray-500" size="24" />
+  </a>
+</template>
