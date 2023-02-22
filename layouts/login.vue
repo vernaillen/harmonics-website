d60a9a398be44455c8e4b45bf2198c6825c501d9
Wouter Vernaillen <wouter@vernaillen.com>
2023-02-22 21:45:13 +0100

    login page updated

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
 <template>
   <navbar-component />
   <div :class="isHydrated ? '' : 'opacity-30 blur-md'" class="flex flex-col min-h-screen">
@@ -6,7 +10,7 @@
         <div class="container mx-auto px-4 pt-[80px] mt-[30px] mb-10 sm:mt-[68px]">
           <div class="flex flex-wrap">
             <div class="w-full lg:w-1/4 px-4 sm:px-6 lg:px-8 mb-5 prose">
-              <h1>Ticket Reservations</h1>
+              <h1>{{ t('login.reservations') }}</h1>
             </div>
             <div class="w-full lg:w-3/4 mb-5 px-4 sm:px-6 lg:px-8 prose">
               <slot />
--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useField, useForm } from 'vee-validate'
 import * as yup from 'yup'
+const { t } = useI18n()
 
 const schema = yup.object({
   email: yup.string().required().email(),
@@ -76,48 +77,53 @@ definePageMeta({
     <Spinner /> logging you in...
   </div>
   <div v-else class="prose m-auto">
-    Confirm your e-mail address to access to your Harmonics reservations<br><br>
-    <button
-      class="bg-[#333] text-white py-1 px-2 mr-2 hover:bg-opacity-80 hover:shadow-signUp rounded-md"
-      @click="signInWithOAuth('github')"
-    >
-      <Icon name="mdi:github" size="28" />
-    </button>
-    <button
-      class="bg-[#1877f2] text-white py-1 px-2 mr-2 hover:bg-opacity-80 hover:shadow-signUp rounded-md"
-      @click="signInWithOAuth('facebook')"
-    >
-      <Icon name="mdi:facebook" size="28" />
-    </button>
-    <button
-      class="bg-[#ea4335] text-white py-1 px-2 mr-2 hover:bg-opacity-80 hover:shadow-signUp rounded-md"
-      @click="signInWithOAuth('google')"
-    >
-      <Icon
-        name="ion:logo-google" size="28"
-      />
-    </button>
-    <br>or<br>
-    <div v-if="emailOptInReply">
-      check your e-mail, the magic link is send to {{ emailAddress }}
+    <p>Bevestig je email adres voor toegang tot Harmonics reservaties:</p>
+    <div class="w-full ml-10 md:w-1/2 lg:w-1/3">
+      <button
+        class="bg-[#1877f2] w-full text-white py-1 px-6 mr-2 my-2 hover:bg-opacity-80 hover:shadow-signUp rounded-lg"
+        @click="signInWithOAuth('facebook')"
+      >
+        <Icon name="mdi:facebook" size="28" />
+        {{ t('login.facebook') }}
+      </button><br>
+      <button
+        class="bg-[#ea4335] w-full text-white py-1 px-6 mr-2 my-2 hover:bg-opacity-80 hover:shadow-signUp rounded-lg"
+        @click="signInWithOAuth('google')"
+      >
+        <Icon name="ion:logo-google" size="28" />
+        {{ t('login.google') }}
+      </button><br>
+      <button
+        class="bg-[#333] w-full text-white py-1 px-6 mr-2 my-2 hover:bg-opacity-80 hover:shadow-signUp rounded-lg"
+        @click="signInWithOAuth('github')"
+      >
+        <Icon name="mdi:github" size="28" />
+        {{ t('login.github') }}
+      </button>
     </div>
-    <div v-else-if="emailOptInError">
+    <p class="!mt-12">
+      {{ t('login.magiclinkintro') }}:
+    </p>
+    <div v-if="emailOptInReply" class="!ml-10">
+      You've got mail!
+    </div>
+    <div v-else-if="emailOptInError" class="!ml-10">
       emailOptInError: {{ emailOptInError }}
     </div>
-    <div v-else-if="magicLinkPending">
-      <Spinner /> sending you the Magic Link...
+    <div v-else-if="magicLinkPending" class="!ml-10">
+      <Spinner /> {{ t('login.sendingmagiclink') }}
     </div>
-    <div v-else>
+    <div v-else class="!ml-10">
       <form method="POST" @submit.prevent="submitForm">
         <input
           v-model="email" name="email" type="email"
-          class="border rounded-md py-1 px-2 mr-2 mt-1" placeholder="E-mail address"
+          class="border rounded-md py-1 px-2 mr-2 mt-1" :placeholder="t('login.email')"
         >
         <button
           class="bg-primary text-white py-1 px-2 mt-1 hover:bg-opacity-80 hover:shadow-signUp rounded-md"
         >
           <Icon name="mdi:email" />
-          Magic Link
+          {{ t('login.magiclink') }}
         </button>
         <div v-if="emailError" class="text-primary">
           {{ emailError }}
