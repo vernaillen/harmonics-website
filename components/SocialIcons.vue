7ee13e9403f0d96dad187f538e1b4f619a76c060
Wouter Vernaillen <wouter@vernaillen.com>
2023-04-04 11:28:09 +0200

    whatsapp social icon

--- components/SocialIcons.vue
+++ components/SocialIcons.vue
@@ -5,13 +5,13 @@ const iconClass = 'hover:text-primary text-gray-500 ml-2'
 <template>
   <div id="socials" class="min-w-[100px] pt-1 xl:pr-0 mr-[-10px]">
     <NuxtLink aria-label="Instagram" href="https://www.instagram.com/harmonics.be/" target="_blank" rel="me">
-      <Icon name="ph:instagram-logo-light" :class="iconClass" size="22" />
+      <Icon name="ph:instagram-logo" :class="iconClass" size="22" />
     </NuxtLink>
     <NuxtLink aria-label="Facebook" href="https://www.facebook.com/harmonics.be/" target="_blank" rel="me">
-      <Icon name="ph:facebook-logo-light" :class="iconClass" size="22" />
+      <Icon name="ph:facebook-logo" :class="iconClass" size="22" />
     </NuxtLink>
-    <NuxtLink aria-label="YouTube" href="https://www.youtube.com/@harmonicsbe" target="_blank" rel="me">
-      <Icon name="ph:youtube-logo-light" :class="iconClass" size="22" />
+    <NuxtLink aria-label="WhatsApp" href="https://wa.me/+32473344656" target="_blank" rel="me">
+      <Icon name="logos:whatsapp-icon" :class="iconClass" size="22" />
     </NuxtLink>
   </div>
 </template>
