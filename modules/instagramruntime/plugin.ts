ea858d91adb2af78a41b3cc8f2e10e560dac1b7e
Wouter Vernaillen <wouter@vernaillen.com>
2023-04-18 10:27:23 +0200

    lint:fix

--- modules/instagramruntime/plugin.ts
+++ modules/instagramruntime/plugin.ts
@@ -1,13 +1,13 @@
+import { iframeResize } from 'iframe-resizer'
 import { defineNuxtPlugin } from '#app'
-import { iframeResize } from 'iframe-resizer';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('resize', {
-    mounted (el, binding) {
+    mounted(el, binding) {
       el.addEventListener('load', () => iframeResize(binding.value, el))
     },
-    unmounted (el) {
-      el.iFrameResizer.removeListeners();
-    }
+    unmounted(el) {
+      el.iFrameResizer.removeListeners()
+    },
   })
 })
