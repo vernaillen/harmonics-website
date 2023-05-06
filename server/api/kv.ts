db95d634db903c5ca9cf8b34dba0a394c08aa01e
Wouter Vernaillen <wouter@vernaillen.com>
2023-05-06 10:35:57 +0200

    mount kv using database name harmonics-website-redis

--- server/api/kv.ts
+++ server/api/kv.ts
@@ -3,18 +3,18 @@ import vercelDriver from '../unstorage/vercel-driver'
 const storage = useStorage()
 const config = useRuntimeConfig()
 
-storage.mount('test', vercelDriver({
+storage.mount('harmonics-website-redis', vercelDriver({
     url: config.vercelKv.url,
     token: config.vercelKv.token
 }))
 
 export default defineEventHandler(async event => {
-    await storage.setItem('test:thing', {
+    await storage.setItem('harmonics-website-redis:thing', {
         foo: 'bar'
     })
 
-    const retrievedValue = await storage.getItem('test:thing')
-    const keys = await storage.getKeys('test')
+    const retrievedValue = await storage.getItem('harmonics-website-redis:thing')
+    const keys = await storage.getKeys('harmonics-website-redis')
 
     return {
         value: retrievedValue,
