0be4ca891c43f9b1353495e0667c800185694b90
Wouter Vernaillen <wouter@vernaillen.com>
2023-08-21 09:06:19 +0200

    fix test Dutch title

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -34,27 +34,9 @@ describe('test homepage language redirect', () => {
 
 describe('test homepage title section', () => {
   beforeEach(() => {
-    cy.visit('/')
-    // cy.get('#iubenda-cs-banner').should('have.length', 1)
-    // cy.get('#iubenda-cs-banner button.iubenda-cs-accept-btn').should('have.length', 1)
-    // cy.get('#iubenda-cs-banner button.iubenda-cs-accept-btn').first().click()
+    cy.visit('/en')
   })
-  it('test Dutch title section', () => {
-    cy.get('main section#title').should('have.length', 1)
-    cy.get('main section#title h1').should('have.length', 1)
-    cy.get('main section#title h1').first().should('have.text', 'Harmonics')
-    cy.get('main section#title h2').should('have.length', 1)
-    cy.get('main section#title h2').first().should('have.text', 'Genezende kracht van Geluid, Muziek en Dans')
-    cy.get('main section#title p').should('have.length', 1)
-    cy.get('main section#title p').first().should('have.text', 'Trance Dance, Ecstatic Dance, Immersive Soundbaths, Muziek Productie')
-  })
-  it('test English section', () => {
-    cy.get('header .languageSwitcher').should('have.length', 1)
-    cy.get('header .languageSwitcher a').should('have.length', 2)
-    cy.get('header .languageSwitcher a').last().click()
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq('/en')
-    })
+  it('test English title section', () => {
     cy.get('main section#title').should('have.length', 1)
     cy.get('main section#title h1').should('have.length', 1)
     cy.get('main section#title h1').first().should('have.text', 'Harmonics')
@@ -62,6 +44,22 @@ describe('test homepage title section', () => {
     cy.get('main section#title h2').first().should('have.text', 'Healing power of Sound, Music and Dance')
     cy.get('main section#title p').should('have.length', 1)
     cy.get('main section#title p').first().should('have.text', 'Trance Dance, Ecstatic Dance, Immersive Soundbaths, Music Production')
+  })
+  it('test Dutch title section', () => {
+    cy.get('header .languageSwitcher').should('have.length', 1)
+    cy.get('header .languageSwitcher a').should('have.length', 2)
+    cy.get('header .languageSwitcher a').first().contains('NL')
+    cy.get('header .languageSwitcher a').first().click()
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/')
+    })
+    cy.get('main section#title').should('have.length', 1)
+    cy.get('main section#title h1').should('have.length', 1)
+    cy.get('main section#title h1').first().should('have.text', 'Harmonics')
+    cy.get('main section#title h2').should('have.length', 1)
+    cy.get('main section#title h2').first().should('have.text', 'Genezende kracht van Geluid, Muziek en Dans')
+    cy.get('main section#title p').should('have.length', 1)
+    cy.get('main section#title p').first().should('have.text', 'Trance Dance, Ecstatic Dance, Immersive Soundbaths, Muziek Productie')
   })
 })
 
