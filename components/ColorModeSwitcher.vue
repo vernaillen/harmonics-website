a1b2af79fe967d6adfb0570edb83706b8a21ec3b
Wouter Vernaillen <wouter@vernaillen.com>
2023-09-08 19:07:36 +0200

    fix colorMode button

--- components/ColorModeSwitcher.vue
+++ components/ColorModeSwitcher.vue
@@ -1,28 +1,36 @@
+<script setup>
+const colorMode = useColorMode()
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+const showSpinner = ref(true)
+onMounted(() => {
+  showSpinner.value = false
+})
+</script>
+
 <template>
-  <div class="inline-flex w-6 md:w-7">
-    <ClientOnly>
+  <ClientOnly>
+    <UButton
+      size="xs"
+      aria-label="Switch Color Mode"
+      class="z-20"
+      :icon="isDark ? 'i-heroicons-moon' : 'i-heroicons-sun'"
+      @click="isDark = !isDark"
+    />
+    <template #fallback>
       <UButton
+        v-if="showSpinner"
         size="xs"
-        aria-label="Switch to Dark Color Mode"
-        icon="i-heroicons-sun"
-        class="!my-0 cursor-pointer inline-flex dark:hidden"
-        @click="useColorMode().preference = 'dark'"
+        aria-label="Loading Color Mode..."
+        icon="i-ph-spinner"
+        class="z-10 !my-0 inline-flex animate-spin rounded-full"
       />
-      <UButton
-        size="xs"
-        aria-label="Switch to Light Color Mode"
-        icon="i-heroicons-moon"
-        class="!my-0 cursor-pointer hidden dark:inline-flex"
-        @click="useColorMode().preference = 'light'"
-      />
-      <template #fallback>
-        <UButton
-          size="xs"
-          aria-label="Loading Color Mode..."
-          icon="i-ph-spinner"
-          class="!my-0 inline-flex animate-spin rounded-full"
-        />
-      </template>
-    </ClientOnly>
-  </div>
+    </template>
+  </ClientOnly>
 </template>
