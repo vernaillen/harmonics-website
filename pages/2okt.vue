30f1e1acb616631b170d9a805c4876077524f33b
Wouter Vernaillen <wouter@vernaillen.com>
2023-09-09 11:53:09 +0200

    2okt - IG story test

---
+++ pages/2okt.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container prose dark:prose-invert ">
+    <div class="text-center w-72 mx-auto">
+      <h1>
+        Trancedans
+      </h1>
+      <NuxtImg src="/images/harmonics-trance.svg" class="h-24 mx-auto" alt="Harmonics BV" />
+
+      <h2 class="text-primary-500 text-[1.4rem] mt-0 mb-8">
+        Dance your inner freedom
+      </h2>
+      <HorizontalRule class="-mb-4" />
+      <p class="text-sm">
+        maandag 2 oktober om 19u
+      </p>
+      <p class="text-sm">
+        in Zaal Croix ou Pile<br>
+        <span class="text-xs">
+          Saint-Sauveur (nabij Ronse)
+        </span>
+      </p>
+      <HorizontalRule class="-mt-4" />
+    </div>
+  </div>
+</template>
