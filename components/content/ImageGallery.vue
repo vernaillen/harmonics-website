68ca27f0e1706028d94c697f6e22713a93cf95d2
Wouter Vernaillen <wouter@vernaillen.com>
2023-10-12 09:07:09 +0200

    more props for imageGallery: columns & disableLightbox

--- components/content/ImageGallery.vue
+++ components/content/ImageGallery.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 export interface Props {
   folder: string
+  columns?: number
   filter?:string
+  disableLightbox?: boolean
 }
 const props = withDefaults(defineProps<Props>(), {
-  filter: ''
+  filter: '',
+  columns: 3,
+  disableLightbox: false
 })
 
 const lightbox = useLightbox()
@@ -18,19 +22,35 @@ Object.values(import.meta.glob('/public/images/**')).forEach((imageModule) => {
       imageModule.name.indexOf(props.filter) > 0)
   ) { images.push(imageModule.name.substring('/public'.length)) }
 })
-
 function openGallery (i: number) {
-  lightbox.images = images
-  lightbox.visible.value = true
-  lightbox.index.value = i
+  if (!props.disableLightbox) {
+    lightbox.images = images
+    lightbox.visible.value = true
+    lightbox.index.value = i
+  }
 }
+
+const columns = computed(() => {
+  return 'lg:columns-' + props.columns
+})
 </script>
 
 <template>
-  <div class="columns-1 md:columns-2 lg:columns-3 mx-[-8px]">
-    <div v-for="(image, index) in images" :key="index" class="px-2">
-      <div class="imgHoverEffect rounded-xl hover:rounded-2xl overflow-hidden relative mb-8 shadow-md">
-        <NuxtPicture loading="lazy" :src="image" class="cursor-pointer rounded-xl hover:rounded-2xl max-w-full p-0 m-0" :alt="image" @click="openGallery(index)" />
+  <div class="columns-1 md:columns-2 mx-[-8px]" :class="columns">
+    <div v-for="(image, index) in images" :key="index" class="px-1">
+      <div
+        class="rounded-xl hover:rounded-2xl overflow-hidden relative mb-6 shadow-md"
+        :class="disableLightbox ? '' : 'imgHoverEffect'"
+      >
+        <NuxtPicture
+          loading="lazy"
+          :src="image"
+          format="webp"
+          class="rounded-xl hover:rounded-2xl max-w-full p-0 m-0"
+          :class="disableLightbox ? '' : 'cursor-pointer'"
+          :alt="image"
+          @click="disableLightbox ? null : openGallery(index)"
+        />
       </div>
     </div>
   </div>
