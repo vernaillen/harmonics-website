267de2d419ef3d4bc77e239b79243b0ada936605
Wouter Vernaillen <wouter@vernaillen.com>
2023-11-27 11:27:30 +0100

    location De Ringelwikke

---
+++ components/content/GoogleMapsDeRingelwikke.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <div>
+    <NuxtLink href="https://maps.app.goo.gl/ZT6QpLor45CThPKo9" target="_blank" aria-label="google maps">
+      <UIcon name="i-mdi-google-maps" /> google maps
+    </NuxtLink><br>
+  </div>
+</template>
