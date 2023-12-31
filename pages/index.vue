15bdb46379b1eec48654cf4e5535008e8fd4db34
Wouter Vernaillen <wouter@vernaillen.com>
2023-12-31 09:38:09 +0100

    layout for home

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,25 @@
 defineOgImage({
   component: 'OGImageHome'
 })
+definePageMeta({
+  layout: 'home'
+})
 </script>
 
 <template>
-  <main class="flex-grow">
-    <div class="w-full container mx-auto py-4">
-      <div class="prose prose-primary dark:prose-invert text-center">
-        <ContentDoc />
-      </div>
-    </div>
-  </main>
+  <div>
+    <NuxtLayout>
+      <main class="flex-grow">
+        <div class="w-full container mx-auto py-4">
+          <div class="prose prose-primary dark:prose-invert text-center">
+            <ContentDoc v-slot="{ doc }">
+              <ContentRenderer v-if="doc" :value="doc">
+                <ContentRendererMarkdown :value="doc" />
+              </ContentRenderer>
+            </ContentDoc>
+          </div>
+        </div>
+      </main>
+    </NuxtLayout>
+  </div>
 </template>
