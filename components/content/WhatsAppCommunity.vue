7c43efa4572b14e12b2fcad5e56005bd19a4815b
Wouter Vernaillen <wouter@vernaillen.com>
2024-01-24 10:29:32 +0700

    WhatsApp community

--- components/content/WhatsAppCommunity.vue
+++ components/content/WhatsAppCommunity.vue
@@ -1,10 +1,6 @@
-<script setup lang="ts">
-defineProps<{ nr: string, size?: string }>()
-</script>
-
 <template>
   <UButton
-    :size="size ? size : 'xs'"
+    size="xs"
     icon="i-logos-whatsapp-icon"
     to="https://chat.whatsapp.com/Jj1VjJkzKzg6vbMtHp58hu"
     target="_blank"
